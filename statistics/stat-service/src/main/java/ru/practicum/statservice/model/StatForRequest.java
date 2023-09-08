ee2f14cc7b126d960775eb5507c0049b410f6115
EvgeniyTomilov <Tes-105015>
2023-09-08 20:53:59 +0300

    fix bag

--- statistics/stat-service/src/main/java/ru/practicum/statservice/model/StatForRequest.java
+++ statistics/stat-service/src/main/java/ru/practicum/statservice/model/StatForRequest.java
@@ -7,8 +7,8 @@ import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.Future;
+import javax.validation.constraints.FutureOrPresent;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.PastOrPresent;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -21,13 +21,12 @@ public class StatForRequest {
     public static final String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
     @DateTimeFormat(pattern = DATE_FORMAT)
     @NotNull
-    @PastOrPresent
+    @FutureOrPresent
     private LocalDateTime start;
     @DateTimeFormat(pattern = DATE_FORMAT)
     @NotNull
     @Future
     private LocalDateTime end;
-    @NotNull
     private List<String> uris = new ArrayList<>();
     @NotNull
     private Boolean unique = false;
